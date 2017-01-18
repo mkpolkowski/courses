9d47761532af2026c4961e83e01bfff8b761cbd3
Mateusz Polkowski <mkpolkowski@gmail.com>
2017-01-18 13:14:54 +0100

    Update and rename mario.c to mario_less.c

--- cs50/pset1/mario_less.c
+++ cs50/pset1/mario_less.c
@@ -3,17 +3,21 @@
 
 int main(void) {
     int height;
-    int retry = 3;
+    int retry = 2;
+
+    // ask for a number until user gives value between 0 and 23
     do {
         if (retry > 0) {
-            printf("Height: ");
+            printf("Height: (%i)", retry);
         } else {
-            printf("Retry: ");
+            printf("Retry: (%i)", retry);
+            retry = 3;
         }
         height = GetInt();
         retry--;
     } while (height < 0 || height > 23);
 
+    // draw the piramid
     for (int j = 0; j < height; j++) {
         for (int i = height - 1; i >= 0; i--) {
             if (i > j) {
