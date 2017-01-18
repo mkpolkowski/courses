34b2897cb12862db59241bdd3b80280ee2e873e4
Mateusz Polkowski <mkpolkowski@gmail.com>
2017-01-18 12:57:00 +0100

    Update water.c

--- cs50/pset1/water.c
+++ cs50/pset1/water.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 
 int main(void) {
+    // declare a variable
     int time_in_minutes;
+
+    // ask user for number of minutes
     printf("minutes: ");
     time_in_minutes = GetInt();
+    
+    // print number of bottles generated with the time
     printf("bottles: %d\n", time_in_minutes * 12);
 }
